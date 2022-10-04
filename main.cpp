e3f8f1f7ef5e8c4deb9d614caecec695a3ecbd81
Aki_School_PC <1916043@PC64228>
2022-10-04 14:25:33 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "DxLib.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(true);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (ProcessMessage()==0)
+	{
+		ClsDrawScreen();
+		DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+		ScreenFlip();
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
